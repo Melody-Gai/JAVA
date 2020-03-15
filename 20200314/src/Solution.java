ec39e162467960bc25197a48018c25fbdd808a22
Melody-Gai <539263156@qq.com>
2020-03-15 22:05:00 +0800

    力扣练习

--- 20200314/src/Solution.java
+++ 20200314/src/Solution.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 public class Solution {
-    public int[] multiply(int[] A) {
+    /*public int[] multiply(int[] A) {
         int[] B = new int[A.length];
         for (int i = 0; i < A.length; i++) {
             B[i] =
         }
-    }
+    }*/
 }
