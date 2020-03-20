214f9921aab3bb1104a20ab55037c9c33452091e
Melody-Gai <539263156@qq.com>
2020-03-20 23:24:07 +0800

    IO

--- 20200320/src/Main.java
+++ 20200320/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+            if (N == 0) {
+                return;
+            }
+            int[] arr = new int[N];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int T = sc.nextInt();
+            int count = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (T == arr[i]) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
     }
 }
