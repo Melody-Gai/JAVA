a1f05f5f1e357c7e50cfef5c4e29a983de334f75
Melody-Gai <539263156@qq.com>
2020-05-18 22:36:00 +0800

    菜鸟练习

--- 20200515/src/TestDemo.java
+++ 20200515/src/TestDemo.java
@@ -1,14 +1,24 @@
 public class TestDemo {
     public static void main(String args[]) {
-        Thread t = new Thread() {
+        /*Thread t = new Thread() {
             public void run() {
                 pong();
             }
         };
         t.run();
-        System.out.print("ping");
+        System.out.print("ping");*/
+
+        TestDemo cm = new TestDemo();
+        cm.complicatedexpression_r();
+
     }
     static void pong() {
         System.out.print("pong");
+    }
+    public void complicatedexpression_r(){
+        int x=20, y=30;
+        boolean b;
+        b = x > 50 && y > 60 || x > 50 && y < -60 || x < -50 && y > 60 || x < -50 && y < -60;
+        System.out.println(b);
     }
 }
