a721f0101659f82e3b5ba98c25e7a8a4e60bba11
Melody-Gai <539263156@qq.com>
2020-06-09 23:31:37 +0800

    练习

--- 20200602/src/Main.java
+++ 20200602/src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class test1 {
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int x0 = in.nextInt();
-       // in.close();
+        in.close();
         int count = 0;
         while (x0 != 0 && count <= 300000) {
             x0 = ((x0 << 1) + 1) % 1000000007;
